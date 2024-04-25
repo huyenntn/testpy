b1e5e01adddeb1079a9d725de14bc0b4f20694a7
Huyenntn <30312214+huyenntn@users.noreply.github.com>
2024-04-25 14:15:51 +0700

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,122 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config')[env];
+const username = config.database.username
+const password = config.database.password
+const db_host = config.database.host
+const db_port = config.database.port
+const db_name = config.database.name
+const db_url = 'postgres://'+username+':'+password+'@'+db_host+':'+db_port+'/'+db_name
+const localStorage = require('localStorage');
+const util = require('util');
+
+var urls = JSON.parse(localStorage.getItem('urls')) || [];
+var pgp = require('pg-promise')(/* options */)
+
+try{
+    var db = pgp(db_url);
+}catch(err){
+    console.log(err);
+}
+
+exports.main = async function(fullUrl) {
+
+    await db.oneOrNone("select * from public.short_urls where full_url=$1 limit 1",[fullUrl], r => !!r)
+    .then(data => { 
+        if(data == false){
+            var shortUrl = shortenUrl(fullUrl)
+            storeUrl(fullUrl,shortUrl)
+        }else{
+            db.oneOrNone("select * from public.short_urls where full_url=$1 limit 1",[fullUrl],)
+            .then(data => { 
+                console.log("existed: " + data)
+            })
+        }
+    }).catch(error => {
+        console.log(error)
+    })
+
+    await db.oneOrNone("select * from public.short_urls where full_url=$1 limit 1",[fullUrl],)
+    .then(data => {
+        console.log(data.short_url)
+        localStorage.setItem('shortUrl',data.short_url);
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+function shortenUrl() {
+
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var codeLength = 7;
+    var shortenUrl = ''
+
+    for (i=0;i<codeLength;i++){ 
+        shortenUrl += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    return shortenUrl
+
+}
+
+async function storeUrl(fullUrl,shortUrl) {
+    var date = new Date()
+    var created_at = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    date.setMonth(date.getMonth() + 3)
+    var expired_at = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    await db.oneOrNone("select * from public.short_urls where short_url=$1 limit 1",[shortUrl], r => !!r)
+    .then(data => { 
+        if(data == false){
+            db.oneOrNone("insert into public.short_urls(full_url,short_url,hits,created_at,expired_at) values ($1,$2,$3,$4,$5) returning id", [fullUrl, shortUrl, 0, created_at, expired_at])
+            .then(data => {
+                console.log('success: ',data)
+                storeUrlToLocalStorage(fullUrl)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }else{
+            db.oneOrNone("select * from public.short_urls where short_url=$1 limit 1",[shortUrl],)
+            .then(data => { 
+                console.log('success: ',data)
+                storeUrlToLocalStorage(fullUrl)
+                return data
+            })
+        }
+    }).catch(error => {
+        console.log(error)
+    })
+
+}
+
+async function storeUrlToLocalStorage(fullUrl) {
+    
+    await db.oneOrNone("select full_url,short_url,hits from public.short_urls where full_url=$1 limit 1",[fullUrl])
+    .then(data => { 
+        url = {
+            fullUrl: data.full_url,
+            shortUrl: data.short_url,
+            hits: data.hits,
+        }
+        urls.push(url)
+        localStorage.setItem('urls',JSON.stringify(urls))
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+exports.dbCleanup = function() {
+
+    var date = new Date()
+    var current_ts = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    db.any("DELETE FROM public.short_urls WHERE expired_at < NOW()")
+        .then(data => { 
+            console.log("[DBCLEANUP] is running -- current timestamp: " + current_ts)
+        }).catch(error => {
+            console.log("[DBCLEANUP] Error cleaning DB -- current timestamp:  "+current_ts)
+        })
+}
+
+exports.addHits = function(shortUrl) {
+    db.oneOrNone("UPDATE hits SET hits = hits + 1 WHERE shortUrl=$1;",[shortUrl])
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config')[env];
+const handler = require('./handler');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const server = express();
+const CronJob = require('cron').CronJob;
+const localStorage = require('localStorage');
+
+const port = config.server.port;
+const baseUrl = config.server.base_url;
+
+server.engine('html', mustacheExpress());
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static(__dirname + '/public'));
+
+server.post('/shorten', function(req,res) {
+    localStorage.setItem('fullUrl',req.body.fullUrl);
+    handler.main(req.body.fullUrl)
+    setTimeout(function () {
+        res.redirect('/');
+      }, 1000);
+});
+
+server.get('/', function(req,res) {
+    let urls = {
+        "fullUrl": localStorage.getItem('fullUrl'),
+        "shortUrl": localStorage.getItem('shortUrl'),
+        "urls": JSON.parse(localStorage.getItem('urls')),
+        "baseUrl": baseUrl,
+    }
+    res.render('index',urls);
+});
+
+server.listen(port, () => {
+    console.log('server is listening on port: '+port);
+});
+
+new CronJob('0 0 * * *', function() {
+    handler.dbCleanup()
+  }, null, true, 'Asia/Jakarta');
