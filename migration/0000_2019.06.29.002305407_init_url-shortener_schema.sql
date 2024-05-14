2239894e3430bc124582ee881e33e6dc03cd4daa
HuyenNTN <huyenntn.ptit@gmail.com>
2024-05-14 14:30:01 +0700

    update

--- migration/0000_2019.06.29.002305407_init_url-shortener_schema.sql
+++
@@ -1,9 +0,0 @@
-create table "short_urls" (
-  id serial,
-  full_url varchar(255) not null,
-  short_url varchar(255) not null unique,
-  hits int not null,
-  created_at timestamp not null,
-  expired_at timestamp not null,
-  primary key (id)
-);
